3b5ad7a17569bb7df573936781510bd8226c700b
Stuart McGill <stuart@global-initiative.com>
2024-03-30 00:06:20 +0000

    Add sample file

--- pages/Listen.vue
+++ pages/Listen.vue
@@ -12,17 +12,8 @@ onMounted(() => {
 
 <template>
   <h1>Listen to a Cicipu folktale</h1>
-  <audio
-    id="audio"
-    ref="audio"
-    controls
-    @timeupdate="handleTimeUpdate"
-    @seeked="handleTimeUpdate"
-  >
-    <source
-      src="https://filesamples.com/samples/audio/mp3/Symphony%20No.6%20(1st%20movement).mp3"
-      type="audio/mpeg"
-    />
+  <audio id="audio" ref="audio" controls @timeupdate="handleTimeUpdate">
+    <source src="/audio/symphony.mp3" type="audio/mpeg" />
     Your browser does not support the audio tag.
   </audio>
   <div class="mt-4 flex items-center gap-4">
