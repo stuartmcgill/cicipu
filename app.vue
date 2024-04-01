34e72ecc208e10e6d6e026241affb72d9124b6ad
Stuart McGill <stuart@global-initiative.com>
2024-04-01 16:10:09 +0100

    Add copyright footer

--- app.vue
+++ app.vue
@@ -1,10 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const currentYear = new Date().getFullYear()
+</script>
 
 <template>
-  <div class="mx-auto container max-w-5xl">
+  <div class="mx-auto container flex flex-col max-w-5xl min-h-screen">
     <NavBar />
-    <div class="mt-12 mx-2 sm:mx-4">
-      <NuxtPage />
+    <div class="mb-4 mx-2 sm:mx-4 grow">
+      <NuxtPage class="my-12" />
     </div>
+    <footer class="mb-4 mx-2 sm:mx-4 flex justify-end">
+      &copy; {{ currentYear }}
+    </footer>
   </div>
 </template>
