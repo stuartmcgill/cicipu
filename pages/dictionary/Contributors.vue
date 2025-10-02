8e79ff4fd0113ce50f76704f3136c79ced8dc6f4
Stuart McGill <stueymcgill@gmail.com>
2025-10-02 20:51:47 +0100

    Add link to contributor page

--- pages/dictionary/Contributors.vue
+++ pages/dictionary/Contributors.vue
@@ -2,6 +2,13 @@
 import { useAppStore } from '~/stores/app.js'
 import type { TableRow } from '#ui/types'
 
+interface ContributorRow {
+  id: number
+  name: string
+  exampleCount: number
+  languages: string
+}
+
 definePageMeta({
   layout: 'dictionary'
 })
@@ -12,9 +19,9 @@ appStore.backgroundImage = ''
 const store = useDictionaryStore()
 
 const columns = [
-  { key: 'name', label: 'Name' },
-  { key: 'exampleCount', label: 'Example count' },
-  { key: 'languages', label: 'Languages spoken' }
+  { key: 'name', label: 'Name', sortable: true },
+  { key: 'exampleCount', label: 'Example count', sortable: true },
+  { key: 'languages', label: 'Languages spoken', sortable: true }
 ]
 const rows: Ref<TableRow[]> = ref([])
 
@@ -24,16 +31,22 @@ onMounted(async () => {
   console.log(contributors)
 
   rows.value = contributors.map((contributor) => ({
+    id: contributor.id,
     name: contributor.name,
     exampleCount: contributor.exampleCount,
     languages: contributor.languages.map((language) => language.name).join(', ')
   }))
 })
+
+const selectContributor = (contributor: ContributorRow) => {
+  const router = useRouter()
+  router.push(`/dictionary/Contributors/${contributor.id}`)
+}
 </script>
 
 <template>
   <div class="mx-auto max-w-2xl text-lg">
     <h1>Contributors</h1>
-    <UTable :rows="rows" :columns="columns" />
+    <UTable :rows="rows" :columns="columns" @select="selectContributor" />
   </div>
 </template>
